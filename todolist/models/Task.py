775913558e1b3b6d9efe1a7a1ce190eb709acee7
Nhurbis <144697062+Nhurbis@users.noreply.github.com>
2023-10-10 17:40:46 +0200

    Commit number 1

--- todolist/models/Task.py
+++ todolist/models/Task.py
@@ -1,7 +1,19 @@
 from datetime import datetime
 
+class TagError(Exception):
+    pass
+
 class Task:
-    def __init__(self:str, name:str, description, tags=None):
+    def __init__(self, name:str, description:str, tags=None):
+        if not isinstance(name, str) or not isinstance(description, str):
+            debug_logger.debug("Le nom et la description doivent être des chaînes de caractères.")
+            raise TypeError("Le nom et la description doivent être des chaînes de caractères.")
+            
+        if not name or not description:
+            debug_logger.debug("Le nom et la description ne peuvent pas être vides.")
+            raise ValueError("Le nom et la description ne peuvent pas être vides.")
+               
+
         self.name = name
         self.description = description
         self.created_at = datetime.now()
@@ -11,23 +23,57 @@ class Task:
         self.tags = tags if tags else []
 
     def mark_completed(self):
+        if self.completed:
+            debug_logger.debug("La tâche est déjà marquée comme complétée.")
+            raise TaskAlreadyCompletedError("La tâche est déjà marquée comme complétée.")
+
         self.completed = True
         self.completion_date = datetime.now()
 
     def update_task(self, new_name=None, new_description=None):
         if new_name:
+            if not isinstance(new_name, str):
+                debug_logger.debug("Le nouveau nom doit être une chaîne de caractères.")
+                raise TypeError("Le nouveau nom doit être une chaîne de caractères.")
+                        
+            general_logger.info("Modification du nom d'une tâche : " + self.name + " -> " + new_name)
             self.name = new_name
-        if new_description:
-            self.description = new_description
-        self.last_modified = datetime.now()
+            self.last_modified = datetime.now()
 
-    def add_tag(self, tag):
+        if new_description:
+            if not isinstance(new_description, str):
+                debug_logger.debug("La nouvelle description doit être une chaîne de caractères.")
+                raise TypeError("La nouvelle description doit être une chaîne de caractères.")
+                        
+            general_logger.info("Modification de la description d'une tâche : " + self.name + " -> " + new_description)
+            self.description = new_description
+            self.last_modified = datetime.now()
+        if not new_name and not new_description:
+            debug_logger.debug("Aucun paramètre de modification n'a été fourni.")
+            raise ValueError("Aucun paramètre de modification n'a été fourni.")
+
+    def add_tag(self, tag:str):
+        if not isinstance(tag, str):
+            debug_logger.debug("Le tag doit être une chaîne de caractères.")
+            raise TypeError("Le tag doit être une chaîne de caractères.")
+        
+        if tag in self.tags:
+            debug_logger.debug(f"Le tag '{tag}' existe déjà pour cette tâche.")
+            raise TagError(f"Le tag '{tag}' existe déjà pour cette tâche.")
+            
+        
         if tag not in self.tags:
+            general_logger.info("Ajout d'un tag à une tâche : " + self.name + " -> " + tag)
             self.tags.append(tag)
             self.last_modified = datetime.now()
 
-    def remove_tag(self, tag):
+    def remove_tag(self, tag:str):
+        if tag not in self.tags:
+            debug_logger.debug(f"Le tag '{tag}' n'existe pas pour cette tâche.")
+            raise TagError(f"Le tag '{tag}' n'existe pas pour cette tâche.")
+         
         if tag in self.tags:
+            general_logger.info("Suppression d'un tag à une tâche : " + self.name + " -> " + tag)
             self.tags.remove(tag)
             self.last_modified = datetime.now()
 
--- todolist/models/TaskList.py
+++ todolist/models/TaskList.py
@@ -1,29 +1,54 @@
 
+class TaskAlreadyExistsError(Exception):
+    pass
+
+class TaskNotFoundError(Exception):
+    pass
+
 class TaskList:
     def __init__(self):
         self.tasks = {}
 
-    def add_task(self, name, description, tags=None):
+    def add_task(self, name:str, description:str, tags=None):
+        if not isinstance(name, str):
+            debug_logger.debug("Le nom doit être des chaînes de caractères.")
+            raise TypeError("Le nom et la description doivent être des chaînes de caractères.")
+        
+        if not name:
+            debug_logger.debug("Le nom ne peut pas être vide.")
+            raise ValueError("Le nom ne peut pas être vide.")
+        
         if name in self.tasks:
-            print("Cette tâche existe déjà.")
-            return
+            debug_logger.debug("La tâche existe déjà.")
+            general_logger.info("Tentative d'ajout d'une tâche existante : " + name)
+            raise TaskAlreadyExistsError(f"La tâche '{name}' existe déjà.")
+
+        general_logger.info("Ajout d'une nouvelle tâche : " + name)
         self.tasks[name] = Task(name, description, tags)
 
-    def complete_task(self, name):
+    def complete_task(self, name:str):
         if name not in self.tasks:
-            print("Cette tâche n'existe pas.")
-            return
+            debug_logger.debug("La tâche n'existe pas.")
+            general_logger.info("Tentative de complétion d'une tâche inexistante : " + name)
+            raise TaskNotFoundError(f"La tâche '{name}' n'existe pas.")
+        
+        general_logger.info("Complétion d'une tâche : " + name)
         self.tasks[name].mark_completed()
 
-    def remove_task(self, name):
+    def remove_task(self, name:str):
         if name not in self.tasks:
-            print("Cette tâche n'existe pas.")
-            return
+            debug_logger.debug("La tâche n'existe pas.")
+            general_logger.info("Tentative de suppression d'une tâche inexistante : " + name)
+            raise TaskNotFoundError(f"La tâche '{name}' n'existe pas.")
+        
+        general_logger.info("Suppression d'une tâche : " + name)
         del self.tasks[name]
 
     def display_tasks(self):
         if not self.tasks:
+            general_logger.info("Tentative d'affichage d'une liste de tâches vide.")
             print("Aucune tâche à afficher.")
             return
+        general_logger.info("Affichage d'une liste de tâches.")
         for name, task in self.tasks.items():
             print(task)
