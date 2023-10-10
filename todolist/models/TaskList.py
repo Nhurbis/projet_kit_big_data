a157a525b57160ec7e1efb272a6feadd251381bb
Lucas <wakuucomptes@gmail.com>
2023-10-10 17:27:11 +0200

    Ajout de la bdd mongo

--- todolist/models/TaskList.py
+++ todolist/models/TaskList.py
@@ -1,29 +1,37 @@
+from todolist.config.db_config import db
+from datetime import datetime
+from todolist.models.Task import Task
+
 
 class TaskList:
     def __init__(self):
-        self.tasks = {}
+        self.collection = db.tasks  # Suppose que vous avez une collection nommée "tasks"
 
     def add_task(self, name, description, tags=None):
-        if name in self.tasks:
+        if self.collection.find_one({"name": name}):
             print("Cette tâche existe déjà.")
             return
-        self.tasks[name] = Task(name, description, tags)
+        task = Task(name, description, tags)
+        self.collection.insert_one(task.to_dict())
 
     def complete_task(self, name):
-        if name not in self.tasks:
+        if not self.collection.find_one({"name": name}):
             print("Cette tâche n'existe pas.")
             return
-        self.tasks[name].mark_completed()
+        self.collection.update_one(
+            {"name": name}, {"$set": {"completed": True, "completion_date": datetime.now()}})
 
     def remove_task(self, name):
-        if name not in self.tasks:
+        if not self.collection.find_one({"name": name}):
             print("Cette tâche n'existe pas.")
             return
-        del self.tasks[name]
+        self.collection.delete_one({"name": name})
 
     def display_tasks(self):
-        if not self.tasks:
+        tasks = self.collection.find()
+        if not tasks:
             print("Aucune tâche à afficher.")
             return
-        for name, task in self.tasks.items():
+        for task_data in tasks:
+            task = Task.from_dict(task_data)
             print(task)
