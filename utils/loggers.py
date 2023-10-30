3685df2a5d9cdfd79c7a8745cdc738d255b2350b
Nhurbis <144697062+Nhurbis@users.noreply.github.com>
2023-10-30 16:38:37 +0100

    log + exceptions
    log + exceptions + propre ( et quelques remises aux propres)

--- utils/loggers.py
+++ utils/loggers.py
@@ -4,6 +4,8 @@ import os
 """
 This module contains the loggers used in the application.
 """
+
+# Path to the logs folder and the log files (errors and debug)
 directory_path = os.path.dirname(os.path.abspath(__file__)) 
 path_errors = os.path.join(directory_path, "../logs/errors.log")
 path_debug = os.path.join(directory_path, "../logs/debug.log")
@@ -11,17 +13,27 @@ path_debug = os.path.join(directory_path, "../logs/debug.log")
 # Logger's creation for errors
 errors_logger = logging.getLogger('errors_logger')
 errors_logger.setLevel(logging.ERROR)
-errors_file_handler = logging.FileHandler(path_errors, encoding='utf-8')
+try:   
+    errors_file_handler = logging.FileHandler(path_errors, encoding='utf-8')
+except FileNotFoundError:
+    os.mkdir(os.path.join(directory_path, "../logs"))
+    errors_file_handler = logging.FileHandler(path_errors, encoding='utf-8')
+
 errors_formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 errors_file_handler.setFormatter(errors_formatter)
 errors_logger.addHandler(errors_file_handler)
 
+
 # Logger's creation for debug
 debug_logger = logging.getLogger('debug_logger')
 debug_logger.setLevel(logging.INFO)
-debug_file_handler = logging.FileHandler(
-    path_debug, encoding='utf-8')
+try:
+    debug_file_handler = logging.FileHandler(path_debug, encoding='utf-8')
+except FileNotFoundError:
+    os.mkdir(os.path.join(directory_path, "../logs"))
+    debug_file_handler = logging.FileHandler(path_debug, encoding='utf-8')
+    
 debug_formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 debug_file_handler.setFormatter(debug_formatter)
