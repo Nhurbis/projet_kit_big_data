893b579fe88280a2b31a5bfd71c9e9b8c558752a
Nhurbis <144697062+Nhurbis@users.noreply.github.com>
2023-10-31 16:31:11 +0100

    Release v1.0.0 (#62)
    * comment main
    
    dernière modif avant release
    
    * release
    
    pep8 fix
    
    * dzdzd
    
    zdzdazd
    
    * dd
    
    pep8

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from utils.loggers import debug_logger, errors_logger
 if __name__ == "__main__":
     iface = create_gradio_interface()
     try:
+        """ Lance l'application
+        L'application tourne tant que l'utilisateur n'appuie pas sur une touche
+        du clavier ou effectue un Ctrl+C dans le terminal """
         iface.launch(
             server_name="0.0.0.0", inbrowser=True, prevent_thread_lock=True)
         print("Access the application at http://localhost:7860/")
